5020e6296e28d038fd66adf803ee1ec960dd854e
safadtm <safadtm2004@gmail.com>
2023-12-20 14:16:15 +0530

    encode

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,36 @@
-function copyText() {
-    const outputText = document.getElementById('outputText');
-    outputText.select();
-    document.execCommand('copy');
+document.addEventListener('DOMContentLoaded', function () {
+    const encodebtn = document.querySelector('#encodeBt');
+    const decodebtn = document.querySelector('#decodeBt');
+    const key = 3;
 
-}
+    const listLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    let output = '';
+
+    encodebtn.addEventListener('click', () => {
+        const textarea = document.getElementById('inputText');
+        const resultText = document.getElementById('outputText');
+
+
+        for (const letter of textarea.value) {
+            if (!listLetters.includes(letter)) {
+                continue;
+            }
+
+            const indexLetter = listLetters.findIndex((item) => item === letter);
+            let newIndexLetter = indexLetter + key;
+
+            if (newIndexLetter > 25) {
+                newIndexLetter -= 26;
+            }
+
+            output += listLetters[newIndexLetter];
+            resultText.value = output;
+        }
+
+        output = '';
+    })
+
+});
+
+
